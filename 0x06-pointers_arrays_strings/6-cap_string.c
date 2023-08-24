2c3d157d8b68596c9dfe0b1d155e02ea63aafae6
A7med7777 <ahmedhessian56@gmail.com>
2023-08-24 15:06:27 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,40 +3,33 @@
 /**
  * cap_string - capitalizes all words of a string
  * @s: the string to capitalize
- * Return: a pointer to the capitalized string
+ * Return: the capitalized string
  */
 char *cap_string(char *s)
 {
 	int i = 0;
-	char sep[] = " \t\n,;.!?\"(){}";
+	int sep = 1;
+	char c;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z' && (i == 0 || _is_sep(s[i - 1], sep)))
-			s[i] = s[i] - 32;
+		c = s[i];
+		if (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' ||
+				c == '.' || c == '!' || c == '?' || c == '"' || c == '(' ||
+				c == ')' || c == '{' || c == '}')
+		{
+			sep = 1;
+		}
+		else if (sep == 1)
+		{
+			if (c >= 'a' && c <= 'z')
+			{
+				s[i] = c - 32;
+			}
+			sep = 0;
+		}
 		i++;
 	}
-
 	return (s);
-}
-
-/**
- * _is_sep - checks if a character is a separator
- * @c: the character to check
- * @sep: the string of separators
- * Return: 1 if c is a separator, 0 otherwise
- */
-int _is_sep(char c, char *sep)
-{
-	int i = 0;
-
-	while (sep[i] != '\0')
-	{
-		if (c == sep[i])
-			return (1);
-		i++;
-	}
-
-	return (0);
 }
 
