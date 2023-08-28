8f91f1eb73ad2fe1bfbbc8faf95b01a82a57be6e
A7med7777 <ahmedhessian56@gmail.com>
2023-08-29 02:15:38 +0300

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of the substring needle
+ * in the string haystack
+ * @haystack: the string to search in
+ * @needle: the substring to search for
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (haystack + i);
+	}
+
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@
 <br>2-strchr.c: a function that locates a character in a string.
 <br>3-strspn.c: a function that gets the length of a prefix substring.
 <br>4-strpbrk.c: a function that searches a string for any of a set of bytes.
+<br>5-strstr.c: a function that locates a substring.
