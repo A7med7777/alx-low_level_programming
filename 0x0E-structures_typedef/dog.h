f927a6a7c8c3d8d33648839db33a3bd855e22f26
A7med7777 <ahmedhessian56@gmail.com>
2023-09-12 16:15:15 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-/* Define a new type struct dog */
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ *
+ * Description: Define a new type struct dog
+ */
 typedef struct dog
 {
 	char *name;
