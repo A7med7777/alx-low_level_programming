b449840a0424b99e393addc7af680230199e8a3b
A7med7777 <ahmedhessian56@gmail.com>
2023-09-14 17:15:03 -0700

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
