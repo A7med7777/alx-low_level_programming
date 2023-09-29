2cb23683ac357b254461c7d1c174e74b4f112e3b
A7med7777 <ahmedhessian56@gmail.com>
2023-09-29 15:36:01 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to be printed in binary
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+
+	else
+		print_binary_helper(n);
+}
+
+/**
+ * print_binary_helper - prints the binary representation of
+ * a number recursively
+ * @n: the number to be printed in binary
+ */
+void print_binary_helper(unsigned long int n)
+{
+	if (n == 0)
+		return;
+
+	print_binary_helper(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
